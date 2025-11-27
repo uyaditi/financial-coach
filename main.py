cf36c49c53dced4f5d4ebf91bc542f11fc839f3e
aditi.g <aditi.gaikwad@aurionpro.com>
2025-11-27 11:13:51 +0530

    routes for budget and requirements updated

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from routes.budget_routes import router as budget_routes
 app = FastAPI()
 
 app.include_router(budget_routes)
+
+
+if __name__  == "__main__":
+    import uvicorn 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
